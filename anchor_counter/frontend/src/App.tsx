0aa68fb64edb15c90962a78e27658c725a318daa
The-Aethereal <suryanshctf05@gmail.com>
2026-01-02 20:13:30 +0530

    wkdejnu

--- anchor_counter/frontend/src/App.tsx
+++ anchor_counter/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
+} from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   WalletModalProvider,
@@ -10,14 +10,14 @@ import {
 } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import "./App.css";
-import CounterState from "./components/counter-state";
+
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 
+import CounterState from "./components/counter-state";
+
 function App() {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = WalletAdapterNetwork.Devnet;
-  // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
@@ -34,7 +34,8 @@ function App() {
         <WalletModalProvider>
           <WalletMultiButton />
           <h1>Hello Solana</h1>
-          <CounterState/>
+          // ADD THIS LINE
+          <CounterState />
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
--- anchor_counter/frontend/src/anchor/setup.ts
+++ anchor_counter/frontend/src/anchor/setup.ts
@@ -1,23 +1,23 @@
 import type { IdlAccounts } from "@coral-xyz/anchor";
-
 import { Program } from "@coral-xyz/anchor";
-import type { Counter } from "./idl";
+import type { Counter } from "./counter.ts";
+import { Counter as CounterIDL } from "./counter.ts";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
-import CounterState from "../components/counter-state";
-import { IDL } from "@coral-xyz/anchor/dist/cjs/native/system";
 
+// Use the correct program ID from your IDL
 const programId = new PublicKey("Adf9VCdjngGn8f6RZftLNFqc6zVUKs3JF5rvqXzZLwaQ");
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
 // Initialize the program interface with the IDL, program ID, and connection.
 // This setup allows us to interact with the on-chain program using the defined interface.
-export const program = new Program<Counter>(IDL, programId, {
+export const program = new Program<Counter>(CounterIDL as Counter, {
   connection,
 });
 
+// Derive the counter PDA using the same seed as defined in the IDL
 export const [counterPDA] = PublicKey.findProgramAddressSync(
   [Buffer.from("counter")],
-  program.programId,
+  programId,
 );
 
 // This is just a TypeScript type for the Counter data structure based on the IDL
